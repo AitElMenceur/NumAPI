043b9ab4e067c3fe679137c6fba082263cfdc655
AitElMenceur <aitelmenceur@et.esiea.fr>
2020-04-27 10:46:32 +0200

    Liste

--- app/src/main/java/com/example/projetandroid/MainActivity.java
+++ app/src/main/java/com/example/projetandroid/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.projetandroid;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,12 +15,20 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ListAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    private SwipeRefreshLayout refreshLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        refreshLayout = findViewById(R.id.swiperefresh);
 
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Toast.makeText(getApplicationContext(), "refresh", Toast.LENGTH_LONG).show();
+            }
+        });
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
@@ -26,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 100; i++) {
             input.add("Test" + i);
         }
+
         mAdapter = new ListAdapter(input);
         recyclerView.setAdapter(mAdapter);
 
