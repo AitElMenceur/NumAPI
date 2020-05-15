8c75f691add80a2d09cf7cc91ff127e1f1516ba8
AitElMenceur <aitelmenceur@et.esiea.fr>
2020-05-15 11:16:17 +0200

    MVC architecture for MainActivity

--- app/src/main/java/com/example/projetandroid/presentation/view/MainActivity.java
+++ app/src/main/java/com/example/projetandroid/presentation/view/MainActivity.java
@@ -2,42 +2,33 @@ package com.example.projetandroid.presentation.view;
 
 
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
 import com.example.projetandroid.Constant;
 import com.example.projetandroid.R;
-import com.example.projetandroid.data.GerritAPI;
-import com.example.projetandroid.presentation.model.RestNumbersAPI;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
+import com.example.projetandroid.presentation.controller.MainController;
 import java.util.ArrayList;
 import java.util.List;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ListAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
     private SwipeRefreshLayout refreshLayout;
-    private SharedPreferences sharedPreferences;
+
+    MainController controller;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        sharedPreferences = getSharedPreferences(Constant.FACT_PREF, Context.MODE_PRIVATE);
+
         setContentView(R.layout.activity_main);
+
         refreshLayout = findViewById(R.id.swiperefresh);
         refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
@@ -45,10 +36,12 @@ public class MainActivity extends AppCompatActivity {
                 Resfreshdata();
             }
         });
-        ShowList();
+        controller = new MainController(this, getSharedPreferences(Constant.FACT_PREF, Context.MODE_PRIVATE));
+        controller.onStart();
+
     }
 
-    private void ShowList() {
+    public void ShowList() {
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
@@ -61,62 +54,13 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(mAdapter);
     }
 
-
     private void Resfreshdata() {
         refreshLayout.setRefreshing(true);
-        saveAllFact();
+        controller.saveAllFact();
         refreshLayout.setRefreshing(false);
     }
 
-
-    private void APICall(String symbol) {
-        Gson gson = new GsonBuilder()
-                .setLenient()
-                .create();
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(Constant.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build();
-
-        GerritAPI gerritAPI = retrofit.create(GerritAPI.class);
-
-        Call<RestNumbersAPI> call = gerritAPI.GetJson(symbol, "true");
-        call.enqueue(new Callback<RestNumbersAPI>() {
-            @Override
-            public void onResponse(Call<RestNumbersAPI> call, Response<RestNumbersAPI> response) {
-                if (response.isSuccessful() && response.body() != null) {
-                    String Fact = response.body().getFact();
-                    saveaFact(symbol, Fact);
-                } else {
-                    Toast.makeText(getApplicationContext(), "Offline", Toast.LENGTH_LONG).show();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<RestNumbersAPI> call, Throwable t) {
-                showAPIError();
-            }
-
-        });
-
-    }
-
-    private void saveaFact(String symbol, String fact) {
-        int number = Integer.parseInt(symbol);
-        sharedPreferences
-                .edit()
-                .putString("cle_string" + number, fact)
-                .apply();
-    }
-
-
-    private void saveAllFact() {
-        for (int i = 0; i < 200; i++) {
-            APICall(Integer.toString(i));
-        }
-    }
-    private void showAPIError() {
+    public void showAPIError() {
         Toast.makeText(getApplicationContext(), "API Error", Toast.LENGTH_LONG).show();
     }
 }
