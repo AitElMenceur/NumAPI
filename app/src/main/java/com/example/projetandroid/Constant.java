56298d2395ef18ff2775f69b588a073dcedef5c7
AitElMenceur <aitelmenceur@et.esiea.fr>
2020-05-25 12:25:21 +0200

    FireBase Implementation

--- app/src/main/java/com/example/projetandroid/Constant.java
+++ app/src/main/java/com/example/projetandroid/Constant.java
@@ -3,4 +3,5 @@ package com.example.projetandroid;
 public class Constant {
     public static final String FACT_PREF = "Fac_prefs";
     public static final String BASE_URL = "https://numbersapi.p.rapidapi.com/";
+    public static final String CHANNEL_ID = "channel_id";
 }
--- app/src/main/java/com/example/projetandroid/presentation/Singletons.java
+++ app/src/main/java/com/example/projetandroid/presentation/Singletons.java
@@ -2,9 +2,15 @@ package com.example.projetandroid.presentation;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.media.RingtoneManager;
+
+import androidx.core.app.NotificationCompat;
 
 import com.example.projetandroid.Constant;
+import com.example.projetandroid.R;
 import com.example.projetandroid.data.GerritAPI;
+import com.example.projetandroid.presentation.model.MyFirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -15,6 +21,23 @@ public class Singletons {
     private static Gson gsonInstance;
     private static GerritAPI gerritAPIInstance;
     private static SharedPreferences sharedPreferencesInstance;
+    private static NotificationCompat.Builder notificationBuilder;
+
+    public static NotificationCompat.Builder getNotificationBuilder(MyFirebaseMessagingService myFirebaseMessagingService, RemoteMessage remoteMessage) {
+        if (notificationBuilder == null) {
+
+            return new NotificationCompat.Builder(myFirebaseMessagingService, Constant.CHANNEL_ID)
+                    .setContentTitle(remoteMessage.getNotification().getTitle())
+                    .setContentText(remoteMessage.getNotification().getBody())
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setStyle(new NotificationCompat.BigTextStyle())
+                    .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setAutoCancel(true);
+
+        }
+        return notificationBuilder;
+    }
 
     public static GerritAPI getgerritAPIInstance() {
         if (gerritAPIInstance == null) {
